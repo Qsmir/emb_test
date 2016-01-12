160d89aa5de2a1b735e74454acbe1b8b77087d70
Qsmir <robert.kusmirek@netguru.co>
2016-01-12 12:16:15 +0100

    .

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'visitors#staging'
   get 'staging', to: 'visitors#staging'
   get 'production', to: 'visitors#production'
+  get 'single_production', to: 'visitors#single_production'
   get 'staging_production', to: 'visitors#staging_production'
 end
