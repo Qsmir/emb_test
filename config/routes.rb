266056beb746a7689192851eee8dbe620fed3e0b
Qsmir <robert.kusmirek@netguru.co>
2016-01-12 12:03:42 +0100

    new players

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'visitors#staging'
+  get 'staging', to: 'visitors#staging'
+  get 'production', to: 'visitors#production'
+  get 'staging_production', to: 'visitors#staging_production'
 end
